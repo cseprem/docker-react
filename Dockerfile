ee9986fcf3be591c70e8d4b5ee116f043929b32a
Premnath Moorthy <premnath.moorthy@entity.local>
2020-06-25 13:23:46 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:alpine as builder
+WORKDIR '/app'
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+# build folder will have all details to run in prod
+
+
+FROM nginx
+COPY --from=builder /app/build /usr/share/nginx/html
+
+
+
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Hi There Anesh and Aarudhraa, I love you chella Kutties. Hi appee i love you .
         </p>
         <a
           className="App-link"
